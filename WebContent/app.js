c86d15031e061afcbc7d840ff35f694fbbe64614
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-01 11:51:16 +0200

    M+

--- WebContent/app.js
+++ WebContent/app.js
@@ -16,17 +16,31 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function buildContent(hash)
 {
-	// Validamos que empieze por "#"
-	if (!hash.startsWith("#")) return buildError("Page not valid");
-	hash = hash.substring(1);
-	
-	// Miramos si es local o remota y que tenga params válidos
-	let hashParts = hash.split("/");
-	
-	// Miramos tamaño máximo
-	if (hashParts.length > 3)  return buildError("Page definition not valid");
-	
-	return "<h1>" + hash + "</h1><p>Welcome to the page: " + hashParts + "</p>";
+	try
+	{
+		// Validamos que empieze por "#"
+		if (!hash.startsWith("#")) return buildError("Page not valid");
+		hash = hash.substring(1);
+		
+		// Miramos si es local o remota y que tenga params válidos
+		let hashParts = hash.split("/");
+		
+		// Miramos tamaño máximo
+		if (hashParts.length > 3)  return buildError("Page definition not valid");
+		
+		// Miramos partes
+		let stxtUrl = hashParts[0];
+		if (esDominioValido(stxtUrl))				stxtUrl = "https://" + stxtUrl;
+		else if (esNombrePaginaValido(stxtUrl))		stxtUrl = obtenerBaseURL() + "/" + stxtUrl;
+		else										return buildError("Page definition not valid");
+		
+		// Final
+		return "<h1>" + hash + "</h1><p>" + stxtUrl + "</p>";
+	}
+	catch(exception)
+	{
+		return buildError("Page definition not valid");
+	}
 }
 
 function buildError(message)
@@ -46,4 +60,9 @@ function esNombrePaginaValido(pagina)
     // Expresión regular para verificar página válida
     const regexDominio = /^[a-zA-Z0-9-_]+$/;
     return regexDominio.test(pagina);
+}
+
+function obtenerBaseURL() {
+    const url = window.location;
+    return `${url.protocol}//${url.host}`;
 }
