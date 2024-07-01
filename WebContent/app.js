f17f441329a1e97957b329c70121be7438ba630e
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-01 11:33:53 +0200

    M+

--- WebContent/app.js
+++ WebContent/app.js
@@ -16,5 +16,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function buildContent(hash)
 {
-	return "<h1>" + hash + "</h1><p>Welcome to the page: " + hash + "</p>";
+	// Validamos que empieze por "#"
+	if (!hash.startsWith("#")) return buildError("Page not valid");
+	hash = hash.substring(1);
+	
+	// Miramos si es local o remota y que tenga params válidos
+	let hashParts = hash.split("/");
+	
+	// Miramos tamaño máximo
+	if (hashParts.length > 3)  return buildError("Page definition not valid");
+	
+	return "<h1>" + hash + "</h1><p>Welcome to the page: " + hashParts + "</p>";
+}
+
+function buildError(message)
+{
+	return "<h1>" + message + "</h1>"
+}
+
+function esDominioValido(dominio) 
+{
+    // Expresión regular para verificar nombres de dominio válidos
+    const regexDominio = /^([a-zA-Z0-9-_]{1,63}\.)+[a-zA-Z]{2,6}$/;
+    return regexDominio.test(dominio);
+}
+
+function esNombrePaginaValido(pagina) 
+{
+    // Expresión regular para verificar página válida
+    const regexDominio = /^[a-zA-Z0-9-_]+$/;
+    return regexDominio.test(pagina);
 }
