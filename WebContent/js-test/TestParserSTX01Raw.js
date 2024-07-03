fc11c3809747477a9e32a5303c0ceb7c55ffc746
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-03 09:21:50 +0200

    M+

--- WebContent/js-test/TestParserSTX01Raw.js
+++ WebContent/js-test/TestParserSTX01Raw.js
@@ -1,5 +1,6 @@
 import { NamespaceRetriever } from '../js/NamespaceRetriever.js';
 import { STXTParser } from '../js/STXTParser.js';
+import { Parser } from '../js/Parser.js';
 import { Node } from '../js/Node.js';
 import { ParseException } from '../js/ParseException.js';
 import { getUrlContent } from '../js/Utils.js';
@@ -10,10 +11,16 @@ export async function TestParserSTX01Raw()
 	let g_client = await getUrlContent("/namespaces/client.stxt");
 	let g_doc_simple = await getUrlContent("/namespaces/doc_simple.stxt");
 	
+	let parser = new Parser();
+	let doc = (await parser.parse(g_client))[0];
+	
 	// Creamos parser
+	/*
     const namespaceRetriever = new NamespaceRetriever();
+	await namespaceRetriever.addGrammarDefinition(g_client);
 	
-
+	let namespace = await namespaceRetriever.getNameSpace("www.gymstxt.com/client.stxt");
+	*/
 	
-	return g_client + "\n" + g_doc_simple;
+	return g_client + "\n" + g_doc_simple + "\n" + doc.toString();
 }
