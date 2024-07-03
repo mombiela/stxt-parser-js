dea693815b40b9df256c1875e90ff9e43e030ee5
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-03 13:02:53 +0200

    M+

--- WebContent/js-test/TestParserSTX01Raw.js
+++ WebContent/js-test/TestParserSTX01Raw.js
@@ -12,14 +12,17 @@ export async function TestParserSTX01Raw()
 	let g_client = await getUrlContent("/namespaces/client.stxt");
 	let g_doc_simple = await getUrlContent("/namespaces/doc_simple.stxt");
 	
+	/*
 	let parser = new Parser();
 	let doc = (await parser.parse(g_client))[0];
 	let ns = await NamespaceRawTransformer.transformRawNode(doc);
+	*/
 
     const namespaceRetriever = new NamespaceRetriever();
 	await namespaceRetriever.addGrammarDefinition(g_client);
 	await namespaceRetriever.addGrammarDefinition(g_doc_simple);
-	let namespace = await namespaceRetriever.getNameSpace("www.gymstxt.com/client.stxt");
+	let namespace1 = await namespaceRetriever.getNameSpace("www.gymstxt.com/client.stxt");
+	let namespace2 = await namespaceRetriever.getNameSpace("www.dokumentando.com/doc_simple.stxt");
 	
-	return namespace.toString() + "\n" + ns.toString() + "\n" + g_client + "\n" + g_doc_simple + "\n" + doc.toString();
+	return namespace1 + "\n" + namespace2;
 }
