49a8676583d0dd4cb360bc91c684085ef9c91acc
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-03 10:16:58 +0200

    M+

--- WebContent/js/Constants.js
+++ WebContent/js/Constants.js
@@ -1,12 +1,12 @@
-export const Constants = {
-    COMMENT_CHAR: '#',
-    TAB_SPACES: 4,
-    TAB: '\t',
-    SPACE: ' ',
-    SEP_NODE: ':',
-    ENCODING: 'UTF-8',
-    NAMESPACE: 'Namespace'
-};
+export class Constants {
+  static COMMENT_CHAR = '#';
+  static TAB_SPACES = 4;
+  static TAB = '\t';
+  static SPACE = ' ';
+  static SEP_NODE = ':';
+  static ENCODING = 'UTF-8';
+  static NAMESPACE = 'Namespace';
+}
 
 export async function testConstants()
 {
