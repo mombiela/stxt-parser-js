bc4d828c9050fa9a41f9983319a34fdd7ce5f84a
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-09 13:08:55 +0200

    M+

--- WebContent/app/navigation.js
+++ WebContent/app/navigation.js
@@ -1,13 +1,28 @@
 export async function makeNavigation(isDir, hashParts) {
+	let result = {};
+	
 	console.log("ISDIR: " + isDir);
 	console.log("hashParts: " + hashParts);
 	
-	return {
-		hilo_ariadna: [
+	result["hilo_ariadna"] = [
 			{url:"https://www.semantictext.info", descrip:"Otro"},
 			{url:"https://www.google.es", descrip:"Google"},
-		],
-		prev: {url:"https://www.semantictext.info", descrip:"Previa"},
-		next: {url:"https://www.semantictext.info", descrip:"Siguiente"}
-	};
+	];
+	
+	if (!isDir && hashParts.length > 1)
+	{
+		let page = "";
+		for (let i = 0; i<hashParts.length-1; i++)
+		{
+			page = page + "/" + hashParts[i];
+		}
+		page = page + "/index.stxt";
+		console.log("INDEX = " + page);
+		
+		result.prev = {url:"https://www.semantictext.info", descrip:"Previa"};
+		result.next = {url:"https://www.semantictext.info", descrip:"Siguiente"};
+	}
+	
+	
+	return result;
 }
