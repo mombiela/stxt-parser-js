ab3c6807e63a840b973c1ffe4ed5a108d940e6e6
Joan Costa Mombiela <jcostamo@fiatc.es>
2024-07-26 08:12:35 +0200

    Limpieza de código

--- WebContent/app/utils.js
+++
@@ -1,7 +0,0 @@
-export function esDominioValido(dominio) 
-{
-    // Expresión regular para verificar nombres de dominio v�lidos
-    const regexDominio = /^([a-zA-Z0-9-_]{1,63}\.)+[a-zA-Z]{2,6}$/;
-    return regexDominio.test(dominio);
-}
-
