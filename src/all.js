079fc13d79baa2c56312d77306a92d37dc39c922
Joan Costa <joan.costa.mombiela@gmail.com>
2024-08-14 12:55:42 +0200

    M+

--- src/all.js
+++ src/all.js
@@ -1,3 +1,24 @@
+export * from './Constants.js';
+export * from './LineIndent.js';
+export * from './LineSplitter.js';
+export * from './Namespace.js';
+export * from './NamespaceChild.js';
+export * from './NamespaceNode.js';
+export * from './NamespaceRawTransformer.js';
+export * from './NamespaceRetriever.js';
+export * from './NamespaceType.js';
+export * from './NamespaceValidator.js';
+export * from './Node.js';
+export * from './NodeLine.js';
+export * from './ParseException.js';
+export * from './Parser.js';
+export * from './Processor.js';
+export * from './RawCustomProcessor.js';
+export * from './STXTParser.js';
+export * from './STXTProcessor.js';
+export * from './Utils.js';
+
+/*
 import './Constants.js';
 import './LineIndent.js';
 export * from './LineSplitter.js';
@@ -17,4 +38,4 @@ export * from './RawCustomProcessor.js';
 export * from './STXTParser.js';
 export * from './STXTProcessor.js';
 export * from './Utils.js';
-
+*/
